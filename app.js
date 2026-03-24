ed61a31ba528ceaf2b7724a8a4977823dfd56b63
rehman2711 <abdul.r@jaguarsolutions.co.in>
2026-03-24 19:59:17 +0530

    10 request per 1 minutes

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(
 // ==== Rate Limiting Middleware
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes).
+  limit: 10, // Limit each IP to 10 requests per `window` (here, per 1 minutes).
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
